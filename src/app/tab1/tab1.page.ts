93c28639626a09cff7b83772f88305224739a1fd
peerobinz <peerapat.vi@ku.th>
2023-05-15 11:14:16 +0700

    Initial commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { AlertController } from '@ionic/angular';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -7,6 +7,36 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  height!: number;
+  weight!: number;
+  bmi!: number;
 
+  constructor(private alertController: AlertController) {}
+
+  calculateBMI() {
+    const heightMeters = this.height / 100;
+    this.bmi = this.weight / (heightMeters * heightMeters);
+  }
+
+  getResult() {
+    if (this.bmi < 18.5) {
+      return 'Underweight';
+    } else if (this.bmi >= 18.5 && this.bmi < 25) {
+      return 'Normal weight';
+    } else if (this.bmi >= 25 && this.bmi < 30) {
+      return 'Overweight';
+    } else {
+      return 'Obese';
+    }
+  }
+
+  async showResult() {
+    const alert = await this.alertController.create({
+      header: 'BMI Result',
+      message: `Your BMI is ${this.bmi.toFixed(2)} (${this.getResult()})`,
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
-
+import { AlertController } from '@ionic/angular';
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  gender!: string;
+  age!: number;
+  height!: number;
+  weight!: number;
+  bmr!: number;
 
-  constructor() {}
+  constructor(private alertController: AlertController) {}
 
+  calculateBMR() {
+    if (this.gender === 'male') {
+      this.bmr = 88.36 + (13.4 * this.weight) + (4.8 * this.height) - (5.7 * this.age);
+    } else {
+      this.bmr = 447.6 + (9.2 * this.weight) + (3.1 * this.height) - (4.3 * this.age);
+    }
+  }
+
+  async showResult() {
+    const alert = await this.alertController.create({
+      header: 'BMR Result',
+      message: `Your BMR is ${this.bmr.toFixed(2)}`,
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
 }
